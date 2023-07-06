2274f8a109a2a88a983611b8ee32bd0b73eacd88
Akshay <Akshay@AKSHAY>
2023-07-07 02:46:24 +0530

    Add project files.

---
+++ BlazorApp1/Pages/IndexBase.cs
@@ -0,0 +1,35 @@
+﻿using MetaMask.Blazor;
+using MetaMask.Blazor.Exceptions;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorApp1.Pages
+{
+    public class IndexBase : ComponentBase
+    {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        public string MetaMask { get; set; }
+        [Inject]
+        public IMetaMaskService MetaMaskService { get; set; } = default!;
+        protected async Task Connect()
+        {
+             bool hasMetaMask = await MetaMaskService.HasMetaMask();
+             bool isSiteConnected = await MetaMaskService.IsSiteConnected();
+            try
+            {
+                await MetaMaskService.ConnectMetaMask();
+                
+                MetaMask = await MetaMaskService.GetSelectedAddress();
+              
+                NavigationManager.NavigateTo("/home");
+            }
+            catch (UserDeniedException)
+            {
+                //Handle "User Denied" case;
+            }
+            
+        }
+       
+    }
+}
---
+++ BlazorApp1/wwwroot/js/metamaskInterop.js
@@ -0,0 +1,17 @@
+﻿// metamaskInterop.js
+
+window.metamaskInterop = {
+    isMetaMaskInstalled: function () {
+        return typeof window.ethereum !== 'undefined';
+    },
+
+    requestAuthentication: async function () {
+        if (!window.ethereum) {
+            throw new Error('MetaMask is not installed or available.');
+        }
+
+        // Request authentication from MetaMask
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        return accounts;
+    }
+};
